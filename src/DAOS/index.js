7b48d9ac67df4addadcabe76001d2349b4336255
Gibbsforce <75646733+Gibbsforce@users.noreply.github.com>
2021-12-20 19:53:55 -0500

    Delete src directory

--- src/DAOS/index.js
+++
@@ -1,9 +0,0 @@
-// Requiring DAOS User
-const UserDAOMongoDB = require("./users/UserDAOMongoDB");
-
-const DAOS = {}
-
-// User
-if (process.env.STORAGE === "mongodb") DAOS["UserDAO"] = UserDAOMongoDB;
-
-module.exports = DAOS;
--- src/utils/createHash.js
+++
@@ -1,8 +0,0 @@
-const bCrypt = require("bCrypt");
-// Hash password
-const createHash = (password) => bCrypt.hashSync(
-    password,
-    bCrypt.genSaltSync(10),
-    null
-);
-module.exports = createHash;
